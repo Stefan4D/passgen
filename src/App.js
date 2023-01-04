d2e86f543c1e5d63029d41c34b3eef6b81e4a8f7
Stefan <blockchain.crypto.stefan@gmail.com>
2023-01-04 01:08:32 +0000

    Initial Chakra UI components imported

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PasswordGenerator from "./components/PasswordGenerator";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Password Generator</p>
-      </header>
-      <PasswordGenerator />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <p>Password Generator</p>
+        </header>
+        <PasswordGenerator />
+      </div>
+    </ChakraProvider>
   );
 }
 
--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -4,6 +4,46 @@ import { longList } from "../data/eff_large_wordlist"; // 5 dice rolls
 import { shortList } from "../data/eff_short_wordlist_1"; // 4 dice rolls
 import { shortUniqueList } from "../data/eff_short_wordlist_2_0"; // 4 dice rolls
 
+// Chakra UI
+import {
+  CopyIcon,
+  RepeatIcon,
+  InfoIcon,
+  ChatIcon,
+  LockIcon,
+  SettingsIcon,
+  ViewIcon,
+  ViewOffIcon,
+  WarningTwoIcon,
+} from "@chakra-ui/icons";
+import {
+  Textarea,
+  Button,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  FormControl,
+  FormLabel,
+  Switch,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from "@chakra-ui/react";
+
 export default function PasswordGenerator() {
   // Password
   const [includeCaps, setIncludeCaps] = useState(true);
@@ -180,135 +220,230 @@ export default function PasswordGenerator() {
 
   return (
     <div id="generator-container">
-      <div className="passgen app-block">
-        <form onSubmit={handlePasswordSubmit}>
-          <div className="password-output" id="password-output">
-            {password}
-          </div>
-          <div className="entropy">{passwordEntropy}</div>
-          <button>Generate Password</button>
-          <label htmlFor="password-length">Password Length</label>
-          <input
-            id="password-length"
-            type="range"
-            min="5"
-            max="99"
-            defaultValue="16"
-            onChange={(e) => setPasswordLength(e.target.value)}
-          />
-          <span>{passwordLength}</span>
-          <fieldset>
-            <legend>Character options:</legend>
-            <label htmlFor="caps">Uppercase Letters</label>
-            <input
-              type="checkbox"
-              name="caps"
-              id="caps"
-              defaultChecked
-              onChange={(e) => setIncludeCaps(e.target.checked)}
-            />
-            <label htmlFor="lower">Lowercase Letters</label>
-            <input
-              type="checkbox"
-              name="lower"
-              id="lower"
-              defaultChecked
-              onChange={(e) => setIncludeLower(e.target.checked)}
-            />
-            <label htmlFor="nums">Numbers</label>
-            <input
-              type="checkbox"
-              name="nums"
-              id="nums"
-              defaultChecked
-              onChange={(e) => setIncludeNumbers(e.target.checked)}
-            />
-            <label htmlFor="special">Special Characters</label>
-            <input
-              type="checkbox"
-              name="special"
-              id="special"
-              defaultChecked
-              onChange={(e) => setIncludeSpecial(e.target.checked)}
-            />
-          </fieldset>
-        </form>
-      </div>
-      <br />
-      <div className="passphrasegen app-block">
-        <form onSubmit={handlePassphraseSubmit}>
-          <div className="passphrase-output" id="passphrase-output">
-            {passphrase}
-          </div>
-          <button>Generate Passphrase</button>
-          <label htmlFor="passphrase-length">Passphrase Length</label>
-          <input
-            id="passphrase-length"
-            type="range"
-            min="3"
-            max="32"
-            defaultValue="5"
-            onChange={(e) => setPassphraseNumWords(e.target.value)}
-          />
-          <span>{passphraseNumWords} words</span>
-          <fieldset>
-            <legend>Select number of dice:</legend>
+      <Tabs>
+        <TabList>
+          <Tab>Password</Tab>
+          <Tab>Passphrase</Tab>
+        </TabList>
 
-            <div>
-              <input
-                type="radio"
-                id="four-dice"
-                name="num-dice"
-                value="4"
-                onChange={(e) => {
-                  setNumDice(e.target.value);
-                  setWordList(shortList);
-                }}
-              />
-              <label htmlFor="four-dice">4 dice (simple)</label>
+        <TabPanels>
+          <TabPanel>
+            <div className="passgen app-block">
+              <form onSubmit={handlePasswordSubmit}>
+                <div className="password-output" id="password-output">
+                  {password}
+                </div>
+                <div className="entropy">{passwordEntropy}</div>
+                <Button onClick={handlePasswordSubmit}>
+                  Generate Password
+                </Button>
+                <label htmlFor="password-length">Password Length</label>
+                <input
+                  id="password-length"
+                  type="range"
+                  min="5"
+                  max="99"
+                  defaultValue="16"
+                  onChange={(e) => setPasswordLength(e.target.value)}
+                />
+                <span>{passwordLength}</span>
+
+                <Accordion allowToggle>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box as="span" flex="1" textAlign="left">
+                          Advanced Options
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <fieldset>
+                        <legend>Character options:</legend>
+                        <label htmlFor="caps">Uppercase Letters</label>
+                        <input
+                          type="checkbox"
+                          name="caps"
+                          id="caps"
+                          defaultChecked
+                          onChange={(e) => setIncludeCaps(e.target.checked)}
+                        />
+                        <label htmlFor="lower">Lowercase Letters</label>
+                        <input
+                          type="checkbox"
+                          name="lower"
+                          id="lower"
+                          defaultChecked
+                          onChange={(e) => setIncludeLower(e.target.checked)}
+                        />
+                        <label htmlFor="nums">Numbers</label>
+                        <input
+                          type="checkbox"
+                          name="nums"
+                          id="nums"
+                          defaultChecked
+                          onChange={(e) => setIncludeNumbers(e.target.checked)}
+                        />
+                        <label htmlFor="special">Special Characters</label>
+                        <input
+                          type="checkbox"
+                          name="special"
+                          id="special"
+                          defaultChecked
+                          onChange={(e) => setIncludeSpecial(e.target.checked)}
+                        />
+                      </fieldset>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </form>
             </div>
+          </TabPanel>
 
-            <div>
-              <input
-                type="radio"
-                id="four-dice"
-                name="num-dice"
-                value="4"
-                onChange={(e) => {
-                  setNumDice(e.target.value);
-                  setWordList(shortUniqueList);
-                }}
-              />
-              <label htmlFor="four-dice">4 dice</label>
+          <TabPanel>
+            <div className="passphrasegen app-block">
+              <form onSubmit={handlePassphraseSubmit}>
+                <div className="passphrase-output" id="passphrase-output">
+                  {passphrase}
+                </div>
+                <Button onClick={handlePassphraseSubmit}>
+                  Generate Passphrase
+                </Button>
+                <label htmlFor="passphrase-length">Passphrase Length</label>
+                <input
+                  id="passphrase-length"
+                  type="range"
+                  min="3"
+                  max="32"
+                  defaultValue="5"
+                  onChange={(e) => setPassphraseNumWords(e.target.value)}
+                />
+                <span>{passphraseNumWords} words</span>
+
+                <Accordion allowToggle>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box as="span" flex="1" textAlign="left">
+                          Advanced Options
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <fieldset>
+                        <legend>Select number of dice:</legend>
+
+                        <div>
+                          <input
+                            type="radio"
+                            id="four-dice"
+                            name="num-dice"
+                            value="4"
+                            onChange={(e) => {
+                              setNumDice(e.target.value);
+                              setWordList(shortList);
+                            }}
+                          />
+                          <label htmlFor="four-dice">4 dice (simple)</label>
+                        </div>
+
+                        <div>
+                          <input
+                            type="radio"
+                            id="four-dice"
+                            name="num-dice"
+                            value="4"
+                            onChange={(e) => {
+                              setNumDice(e.target.value);
+                              setWordList(shortUniqueList);
+                            }}
+                          />
+                          <label htmlFor="four-dice">4 dice</label>
+                        </div>
+
+                        <div>
+                          <input
+                            type="radio"
+                            id="five-dice"
+                            name="num-dice"
+                            value="5"
+                            defaultChecked
+                            onChange={(e) => {
+                              setNumDice(e.target.value);
+                              setWordList(longList);
+                            }}
+                          />
+                          <label htmlFor="five-dice">5 dice</label>
+                        </div>
+                      </fieldset>
+
+                      <FormControl display="flex" alignItems="center">
+                        <FormLabel htmlFor="passphrase-separator" mb="0">
+                          Passphrase Separator
+                        </FormLabel>
+                        <Input
+                          type="text"
+                          name="separator"
+                          id="passphrase-separator"
+                          defaultValue="-"
+                          maxLength={2}
+                          width={20}
+                          onChange={(e) =>
+                            setPassphraseSeparator(e.target.value)
+                          }
+                        />
+                      </FormControl>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </form>
             </div>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
 
-            <div>
-              <input
-                type="radio"
-                id="five-dice"
-                name="num-dice"
-                value="5"
-                defaultChecked
-                onChange={(e) => {
-                  setNumDice(e.target.value);
-                  setWordList(longList);
-                }}
-              />
-              <label htmlFor="five-dice">5 dice</label>
-            </div>
-          </fieldset>
+      <Textarea placeholder={passphrase} size="lg" />
+      <Button colorScheme="blue">Button</Button>
+      <CopyIcon boxSize={6} />
+      <RepeatIcon boxSize={6} />
+      <InfoIcon boxSize={6} />
+      <ChatIcon boxSize={6} />
+      <LockIcon boxSize={6} />
+      <SettingsIcon boxSize={6} />
+      <ViewIcon boxSize={6} />
+      <ViewOffIcon boxSize={6} />
+      <WarningTwoIcon boxSize={6} />
 
-          <label htmlFor="passphrase-separator">Passphrase Separator</label>
-          <input
-            type="text"
-            name="separator"
-            id="passphrase-separator"
-            defaultValue="-"
-            maxLength={2}
-            onChange={(e) => setPassphraseSeparator(e.target.value)}
-          />
-        </form>
-      </div>
+      {/* If you add the size prop to `InputGroup`, it'll pass it to all its children. */}
+      <InputGroup size="lg">
+        <InputLeftAddon children={<LockIcon />} />
+        <Input value={password} readOnly />
+        <InputRightAddon children={<CopyIcon />} />
+      </InputGroup>
+
+      <FormControl display="flex" alignItems="center">
+        <FormLabel htmlFor="email-alerts" mb="0">
+          Enable email alerts?
+        </FormLabel>
+        <Switch id="email-alerts" defaultChecked />
+      </FormControl>
+
+      <Slider
+        aria-label="password-length"
+        id="password-length"
+        defaultValue={16}
+        min={5}
+        max={99}
+        step={1}
+      >
+        <SliderTrack bg="red.100">
+          <Box position="relative" right={10} />
+          <SliderFilledTrack bg="tomato" />
+        </SliderTrack>
+        <SliderThumb boxSize={6} />
+      </Slider>
     </div>
   );
 }
